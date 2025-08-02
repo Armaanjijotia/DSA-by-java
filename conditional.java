2cd67295164eece874cb811fa3e59ba2ee26b7a1
Armaanjijotia <armaanjijotia009@gmail.com>
2025-08-02 18:52:36 +0530

    Update conditional.java

--- conditional.java
+++ conditional.java
@@ -1,41 +1,45 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class conditional {
+/**
+ * Demonstrates basic use of conditional statements and switch-case in Java.
+ */
+public class ConditionalExample {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the element for a: ");
+        // --- IF-ELSE COMPARISON ---
+        System.out.print("Enter the value for a: ");
         int a = sc.nextInt();
 
-        System.out.print("Enter the element for b: ");
+        System.out.print("Enter the value for b: ");
         int b = sc.nextInt();
 
-        // IF AND ELSE STATEMENT 
-
-       if (a == b) {
-           System.out.println("Equal");
+        if (a == b) {
+            System.out.println("a is equal to b");
         } else if (a > b) {
             System.out.println("a is greater than b");
         } else {
             System.out.println("a is less than b");
         }
-     
-       // SWITCH CASE 
-       
-       System.out.println("enter compare no.");
-       int compare = sc.nextInt();
-       switch (compare) {
-           case 1:
-           if(compare==1)
-           System.out.println("print a is lesser");
-               break;
+
+        System.out.println();
+
+        // --- SWITCH CASE EXAMPLE ---
+        System.out.print("Enter a number to compare (1 or 2): ");
+        int compare = sc.nextInt();
+
+        switch (compare) {
+            case 1:
+                System.out.println("You selected option 1: a is lesser");
+                break;
             case 2:
-           if(compare==2)
-           System.out.println("print a is greater");
-               break;
-           default:
-           System.out.println("no. invalid");
-              
-       }
+                System.out.println("You selected option 2: a is greater");
+                break;
+            default:
+                System.out.println("Invalid selection.");
+        }
+
+        sc.close(); // Always close your scanner
     }
 }
+
