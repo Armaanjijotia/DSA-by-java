846190e2564e695030f3ed3ee8b373ac9696463d
Armaanjijotia <armaanjijotia009@gmail.com>
2025-08-02 18:51:41 +0530

    Update function.java

--- function.java
+++ function.java
@@ -1,31 +1,51 @@
 import java.util.Scanner;
 
-public class function {
+/**
+ * Demonstrates basic Java functions: printing a name, arithmetic operations, and factorial.
+ */
+public class FunctionExamples {
 
-    // 1. Print user's name
+    /**
+     * Prints the user's name.
+     * @param name The name to print.
+     */
     public static void printMyName(String name) {
         System.out.println("Your name is: " + name);
     }
 
-    // 2. Sum of two numbers
+    /**
+     * Returns the sum of two integers.
+     * @param a First number.
+     * @param b Second number.
+     * @return The sum of a and b.
+     */
     public static int sumOfTwo(int a, int b) {
         int sum = a + b;
         System.out.println("Sum of a and b is: " + sum);
         return sum;
     }
 
-    // 3. Multiplication of two numbers
+    /**
+     * Returns the product of two integers.
+     * @param a First number.
+     * @param b Second number.
+     * @return The product of a and b.
+     */
     public static int multiplyTwo(int a, int b) {
-        int mult = a * b;
-        System.out.println("Multiplication of a and b is: " + mult);
-        return mult;
+        int product = a * b;
+        System.out.println("Multiplication of a and b is: " + product);
+        return product;
     }
 
-    // 4. Factorial of a number
+    /**
+     * Calculates the factorial of a number.
+     * @param n The number.
+     * @return The factorial of n.
+     */
     public static int factorial(int n) {
         int fact = 1;
         for (int i = 1; i <= n; i++) {
-            fact = fact * i;
+            fact *= i;
         }
         System.out.println("Factorial of " + n + " is: " + fact);
         return fact;
@@ -34,20 +54,22 @@ public class function {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Name printing
+        // --- 1. Print user's name ---
         System.out.print("Enter your name: ");
         String name = sc.next();
         printMyName(name);
 
-        // Sum and multiplication
+        // --- 2. Sum and multiplication ---
         System.out.print("Enter number a: ");
         int a = sc.nextInt();
+
         System.out.print("Enter number b: ");
         int b = sc.nextInt();
+
         sumOfTwo(a, b);
         multiplyTwo(a, b);
 
-        // Factorial
+        // --- 3. Factorial ---
         System.out.print("Enter number n to find factorial: ");
         int n = sc.nextInt();
         factorial(n);
@@ -55,3 +77,4 @@ public class function {
         sc.close(); // Always good practice to close Scanner
     }
 }
+
