b16d3aca964f66d425ac7f8b7b23bf350b0957cf
Armaanjijotia <armaanjijotia009@gmail.com>
2025-08-02 18:53:55 +0530

    Update arry.java

--- arry.java
+++ arry.java
@@ -1,67 +1,50 @@
-import java.util.*;
-public class arry {
-    // public static void main(String[] args) {
-    //     int [] marks = new int[5];
-    //      marks[0]=34;
-    //      marks[1]=21;
-    //      marks[2]=44;
-    //      marks[3]=221;
-    //      marks[4]=333;
-    //   for(int i=0;i<5;i++){
-    //     System.out.println(marks[i]);
-    // }
-    //     }
+import java.util.Scanner;
 
+/**
+ * Demonstrates array input, output, and linear search on student names.
+ */
+public class ArrayExample {
 
-    // public static void main(String[] args) {
-    //     Scanner sc = new Scanner(System.in);
-
-    //     System.out.println("Enter the number of students:");
-    //     int numStudents = sc.nextInt();
-    //     sc.nextLine(); // Consume the leftover newline
-
-    //     String[] names = new String[numStudents];
-
-    //     for (int i = 0; i < numStudents; i++) {
-    //         System.out.println("Enter name for student " + (i + 1) + ":");
-    //         names[i] = sc.nextLine();
-    //     }
-
-    //     System.out.println("Names of students:");
-    //     for (int i = 0; i < numStudents; i++) {
-    //         System.out.println(names[i]);
-    //     }
-
-    //     sc.close();
-    // } 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number of students:");
+        // --- Input: Number of students ---
+        System.out.print("Enter the number of students: ");
         int numStudents = sc.nextInt();
-        sc.nextLine(); // Consume the leftover newline
+        sc.nextLine(); // Clear the newline after nextInt()
 
+        // --- Input: Student names ---
         String[] names = new String[numStudents];
-
         for (int i = 0; i < numStudents; i++) {
-            System.out.println("Enter name for student " + (i + 1) + ":");
+            System.out.print("Enter name for student " + (i + 1) + ": ");
             names[i] = sc.nextLine();
-          
         }
-    
-        for (int i = 0; i < numStudents; i++) {
-            System.out.println(names[i]);
-        }
-        System.out.println("Enter name to search:");
-        String x = sc.nextLine();
 
+        // --- Output: All student names ---
+        System.out.println("\nList of students:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        // --- Input: Name to search ---
+        System.out.print("\nEnter name to search: ");
+        String targetName = sc.nextLine();
+
+        // --- Linear search for the name ---
+        boolean found = false;
         for (int i = 0; i < names.length; i++) {
-            if (names[i].equals(x)) {
-                System.out.println("Detected " + i);
-                return;
+            if (names[i].equalsIgnoreCase(targetName)) {
+                System.out.println("Name found at index: " + i);
+                found = true;
+                break;
             }
         }
 
-        System.out.println("Not detected");
+        if (!found) {
+            System.out.println("Name not found.");
+        }
+
+        sc.close(); // Close the Scanner
     }
 }
+
