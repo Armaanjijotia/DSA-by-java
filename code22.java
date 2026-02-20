6d44b5cff9c3e7fddeffe9026fcb1e29226f4010
Armaanjijotia <armaanjijotia009@gmail.com>
2026-02-20 23:48:57 +0530

    Add files via upload

---
+++ code22.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder current,
+                           int open, int close, int n) {
+
+        // If the current string is complete
+        if (current.length() == 2 * n) {
+            result.add(current.toString());
+            return;
+        }
+
+        // Add '(' if we still can
+        if (open < n) {
+            current.append('(');
+            backtrack(result, current, open + 1, close, n);
+            current.deleteCharAt(current.length() - 1);
+        }
+
+        // Add ')' only if valid
+        if (close < open) {
+            current.append(')');
+            backtrack(result, current, open, close + 1, n);
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+}
