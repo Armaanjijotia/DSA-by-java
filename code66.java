175ef2346693fb5b417fc83cc3c3f7f42fd1b7ba
Armaanjijotia <armaanjijotia009@gmail.com>
2026-03-18 00:34:09 +0530

    Add files via upload

---
+++ code66.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class code66 {
+    public int[] plusOne (int[] digits){
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+        }
+        int [] result = new int [digits.length+1];
+        result[0]=1;
+        return result;
+    }
+    
+}
