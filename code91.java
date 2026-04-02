2eb8df76a7675e69e2b77d60400589d7a9e6e5a2
Armaanjijotia <armaanjijotia009@gmail.com>
2026-04-03 00:22:31 +0530

    Add files via upload

---
+++ code91.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        
+        if (n == 0 || s.charAt(0) == '0') return 0;
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            char oneDigit = s.charAt(i - 1);
+            String twoDigits = s.substring(i - 2, i);
+            
+            // Single digit valid
+            if (oneDigit != '0') {
+                dp[i] += dp[i - 1];
+            }
+            
+            // Two digit valid
+            int num = Integer.parseInt(twoDigits);
+            if (num >= 10 && num <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[n];
+    }
+}
