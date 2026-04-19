2c1fb3e7b45fbcf9a76550784072a472f624af69
Armaanjijotia <armaanjijotia009@gmail.com>
2026-04-20 01:43:48 +0530

    Add files via upload

---
+++ code110.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public boolean isBalanced(TreeNode root) {
+        return check(root) != -1;
+    }
+    
+    private int check(TreeNode node) {
+        if (node == null) {
+            return 0; // height = 0
+        }
+        
+        int left = check(node.left);
+        if (left == -1) return -1;
+        
+        int right = check(node.right);
+        if (right == -1) return -1;
+        
+        if (Math.abs(left - right) > 1) {
+            return -1; // not balanced
+        }
+        
+        return Math.max(left, right) + 1;
+    }
+}
