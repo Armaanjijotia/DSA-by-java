401d756a048961db8e96d07ad38868e61e8cf805
Armaanjijotia <armaanjijotia009@gmail.com>
2026-05-23 01:16:47 +0530

    Add files via upload

---
+++ code206.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+
+            ListNode nextNode = current.next; // store next node
+
+            current.next = prev; // reverse link
+
+            prev = current; // move prev forward
+            current = nextNode; // move current forward
+        }
+
+        return prev;
+    }
+}
