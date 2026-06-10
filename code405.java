f01a41419f4f70291a3dd45b010568fedcf60f38
Armaanjijotia <armaanjijotia009@gmail.com>
2026-06-11 01:11:20 +0530

    Add files via upload

---
+++ code405.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+
+        char[] hexChars = {
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+        };
+
+        StringBuilder result = new StringBuilder();
+
+        while (num != 0) {
+            int digit = num & 15; // get last 4 bits
+            result.append(hexChars[digit]);
+            num >>>= 4; // unsigned right shift by 4 bits
+        }
+
+        return result.reverse().toString();
+    }
+}
