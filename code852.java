e8a6c78e1412ea0da3ae80985b58c6af0f2a3336
Armaanjijotia <armaanjijotia009@gmail.com>
2026-07-21 00:06:35 +0530

    Add files via upload

---
+++ code852.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length - 1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
