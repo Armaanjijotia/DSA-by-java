bc79fafa8e71791251740966433d41393b661b4d
Armaanjijotia <armaanjijotia009@gmail.com>
2026-07-26 01:54:41 +0530

    Add files via upload

---
+++ code593.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+
+        int[] d = new int[6];
+        int idx = 0;
+
+        d[idx++] = dist(p1, p2);
+        d[idx++] = dist(p1, p3);
+        d[idx++] = dist(p1, p4);
+        d[idx++] = dist(p2, p3);
+        d[idx++] = dist(p2, p4);
+        d[idx++] = dist(p3, p4);
+
+        Arrays.sort(d);
+
+        return d[0] > 0 &&
+               d[0] == d[1] &&
+               d[1] == d[2] &&
+               d[2] == d[3] &&
+               d[4] == d[5] &&
+               d[4] == 2 * d[0];
+    }
+
+    private int dist(int[] a, int[] b) {
+        int dx = a[0] - b[0];
+        int dy = a[1] - b[1];
+        return dx * dx + dy * dy;
+    }
+}
