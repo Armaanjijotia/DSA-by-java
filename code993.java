7492a56bfc1b02f7a1e2ada623e45f9a5a6d1c1e
Armaanjijotia <armaanjijotia009@gmail.com>
2026-07-31 00:33:58 +0530

    Add files via upload

---
+++ code993.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean foundX = false;
+            boolean foundY = false;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.val == x) foundX = true;
+                if (node.val == y) foundY = true;
+
+                // Check if x and y are siblings
+                if (node.left != null && node.right != null) {
+                    int left = node.left.val;
+                    int right = node.right.val;
+
+                    if ((left == x && right == y) ||
+                        (left == y && right == x)) {
+                        return false;
+                    }
+                }
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            if (foundX && foundY) return true;
+            if (foundX || foundY) return false;
+        }
+
+        return false;
+    }
+}
